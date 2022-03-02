a30308b75dd99b0412f7fc92f2c4635460bc2f74
tatia <tatia.gogashvili708@ens.tsu.edu.ge>
2022-03-02 19:43:30 +0400

    Initial commit

---
+++ src/test/java/ge/example/taro/TaroApplicationTests.java
@@ -0,0 +1,13 @@
+package ge.example.taro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
